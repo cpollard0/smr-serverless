f7255a9c2053049145d99548ada86cc0bba028d8
Chris <chris.pollard@rocketmail.com>
2019-07-27 15:59:10 -0400

    unpipe composer install

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 aws cloudformation deploy --template-file infra.yml --stack-name smr-classic-cmp-test &&
-aws s3 cp ./smr/htdocs s3://images.cmp.smr.cpollard.dev/ --recursive --exclude *.php --exclude *.inc &&
+# aws s3 cp ./smr/htdocs s3://images.cmp.smr.cpollard.dev/ --recursive --exclude *.php --exclude *.inc &&
 aws cloudformation deploy \
     --template-file api_infrastructure.yml \
     --stack-name smr-classic-cmp-api-test \
