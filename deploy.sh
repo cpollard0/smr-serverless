e12a90593aa47c7f4df0c34b25c5b1e273ca3f6d
Chris <chris.pollard@rocketmail.com>
2019-07-28 11:59:43 -0400

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-aws cloudformation deploy --template-file infra.yml --stack-name smr-classic-cmp-test &&
+# aws cloudformation deploy --template-file infra.yml --stack-name smr-classic-cmp-test &&
 # aws s3 cp ./smr/htdocs s3://images.cmp.smr.cpollard.dev/ --recursive --exclude *.php --exclude *.inc &&
 aws cloudformation deploy \
     --template-file api_infrastructure.yml \
