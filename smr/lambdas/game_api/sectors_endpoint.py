8eacd95d71c28928eabd7a1013a191333db52026
Chris <chris.pollard@rocketmail.com>
2019-07-28 17:33:43 -0400

    Update lambda

--- smr/lambdas/game_api/sectors_endpoint.py
+++ smr/lambdas/game_api/sectors_endpoint.py
@@ -1,4 +1,6 @@
 import boto3
+from botocore.exceptions import ClientError
+
 import json
 from decimal import Decimal
 
@@ -24,6 +26,55 @@ def get(gameId, sector_id):
     print(response)
     return response
 
+def updatePort(game_id, sector_id, body):
+    port = {"port": body } 
+    response = table.update_item(
+        Key={
+            'gameId': int(game_id),
+            'sectorId': int(sector_id)
+        },
+        UpdateExpression="SET port_info=(:i)",
+        ExpressionAttributeValues={
+            ':i': body
+        }
+    )  
+    return response
+
+def updatePortGood(game_id, sector_id, body): 
+    del body['sectorId']
+    del body['gameId']  
+    portGood = {"portGoods":{str(body['goodId']):body}} 
+    
+    try:
+        response = table.update_item(
+            Key={
+                'gameId': int(game_id),
+                'sectorId': int(sector_id)
+            }, 
+            
+            UpdateExpression="SET portGoods.#goodNumber=:i", 
+            ExpressionAttributeNames={
+                '#goodNumber' : str(body['goodId']),
+            },
+            ExpressionAttributeValues={
+               ':i': portGood['portGoods'][str(body['goodId'])]
+            }
+        )  
+    
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ValidationException':
+            response = table.update_item(
+            Key={
+                'gameId': int(game_id),
+                'sectorId': int(sector_id)
+            },             
+            UpdateExpression="SET portGoods=:i", 
+            ExpressionAttributeValues={
+                ':i': portGood['portGoods'] 
+            }
+        )  
+    return response
+
 def createUpdate(gameDetails):
     response = table.put_item(
         Item=gameDetails
@@ -32,12 +83,22 @@ def createUpdate(gameDetails):
 
 def handler(event, context): 
     print (event)
-    if event['httpMethod'] == 'GET':
-        response = get(3)
+    if event['resource'] == "/sector/{gameId}/{sectorId}/port": 
+        response = updatePort(event['pathParameters']['gameId'], 
+            event['pathParameters']['sectorId'],
+            json.loads(event['body']))
+    elif event['resource'] == "/sector/{gameId}/{sectorId}/port/good": 
+        response = updatePortGood(event['pathParameters']['gameId'], 
+            event['pathParameters']['sectorId'],
+            json.loads(event['body']))
+        
     else:
-        body = json.loads(event['body'])
-        print(body)
-        response = createUpdate(body)
+        if event['httpMethod'] == 'GET':
+            response = get(3)
+        else:
+            body = json.loads(event['body'])
+            print(body)
+            response = createUpdate(body)
     return {
         "statusCode": 200,
         "body": json.dumps(response, default=handle_decimal_type)
